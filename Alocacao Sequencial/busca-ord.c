7c89e759092a29ae7cebf740bdcd83870b687349
MilenoMatos <mileno.matos@sou.unifal-mg.edu.br>
2025-08-16 00:04:28 -0300

    create busca-ord.c

---
+++ Alocacao Sequencial/busca-ord.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no{ //Struct utilizado para testes de busca
+    int chave;
+    int valor;
+} No;
+
+
+int main(){
+
+
+
+    return 0;
+}
